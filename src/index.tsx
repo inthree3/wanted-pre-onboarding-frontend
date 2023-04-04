689838c21aaf98d3e36b9bdd90df9d4c24e3ac64
inthree3 <inseon_ug@gm.gist.ac.kr>
2023-04-05 01:03:45 +0900

    chore: TS components render 성공

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
